cb6546e422a4975a0bc467a24ade5cf2fcb9511b
puspasri <puspa09111984@gmail.com>
2024-10-29 17:59:00 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,22 @@
+import random
+import math 
+
+a=random.randint(1,10)
+print ("random integers",a )
+b=random.random()
+print ("float",b)
+c=random.choice("computer")
+print("string",c)
+d=math.floor(23.56)
+print("floor",d)
+e=math.ceil(23.56)
+print("ceil",e)
+f=math.copysign(10,-5)
+print("copysign",f)
+g=math.gcd(24,56)
+print("gcd",g)
+h=math.lcm(23,24)
+print("lcm",h)
+
+
+
